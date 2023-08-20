d17b13de191041b91bb21eb31eca6a35d7662d0b
Fatesaid <fatesaid56@gmail.com>
2023-08-20 17:46:45 +0200

    created playRound and getComputerChoice
    fixed output error on getComputerChoice

--- rock.js
+++ rock.js
@@ -1,6 +1,38 @@
-let game = ["Rock", "Paper", "Sissor"];
+let game = ["rock", "paper", "scissor"];
 function getComputerChoice() {
-    console.log(game[(Math.floor(Math.random() * game.length))]);
+    
+    return (game[(Math.floor(Math.random() * game.length))]);
     
 }
-getComputerChoice();
+
+
+ 
+
+const playerSelection = prompt("Choose between 'Rock', 'Paper', or 'Scissor'").toLowerCase();
+const computerSelection = getComputerChoice();
+
+
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection === 'rock' && computerSelection === 'paper') {
+        return ("You lose! Paper beats Rock.")
+    } else if(playerSelection === 'paper' && computerSelection === 'scissor') {
+        return("You lose! Paper beats Rock.")
+    } else if(playerSelection === 'scissor' && computerSelection === 'rock') {
+        return ("You lose! Rock beats Scissor.")
+    } else if(playerSelection === 'rock' && computerSelection === 'scissor') {
+        return("You win! Rock beats Scissors")
+    } else if(playerSelection === 'paper' && computerSelection === 'rock') {
+        return("You win! Paper beats Rock.")
+    } else if(playerSelection === 'scissor' && computerSelection === 'paper') {
+        return("You lose! Paper beats Scissors.")
+    } else if(playerSelection === computerSelection) {
+        return("It's a tie!")
+    } else {
+        return("Wrong input")
+    }
+    
+}
+
+console.log(playRound(playerSelection, computerSelection));
+
