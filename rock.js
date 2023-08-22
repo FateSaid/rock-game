635502f86f732c56574ec8de094befbd59ac8271
Fatesaid <fatesaid56@gmail.com>
2023-08-22 15:17:15 +0200

    restarted project
    code became convoluted so restarted and created functions for getComputerChoice, getPlayerChoice, game, and playRound

--- rock.js
+++ rock.js
@@ -1,47 +1,19 @@
+const choices = ["rock", "paper", "scissor"];
+
+function game(){
+    //5 rounds of rock/paper/scissor; calculate scoreboard
+}
+
+function playRound() {
+    //code for round results
+}
 
 function getComputerChoice() {
-    let game = ["rock", "paper", "scissor"];
-    return (game[(Math.floor(Math.random() * game.length))]);
-    
+    //computer random choice
 }
 
-let playerSelection = prompt("Choose between 'Rock', 'Paper', or 'Scissor'").toLowerCase();
-let computerSelection = getComputerChoice();
- 
-
-function playRound(playerSelection, computerSelection) {
-    if(playerSelection === 'rock' && computerSelection === 'paper') {
-        computerScore++;
-        return ("You lose! Paper beats Rock.")
-    } else if(playerSelection === 'paper' && computerSelection === 'scissor') {
-        computerScore + 1;
-        return("You lose! Paper beats Rock.")
-    } else if(playerSelection === 'scissor' && computerSelection === 'rock') {
-        computerScore + 1;
-        return ("You lose! Rock beats Scissor.")
-    } else if(playerSelection === 'rock' && computerSelection === 'scissor') {
-        userScore + 1;;
-        return("You win! Rock beats Scissors")
-    } else if(playerSelection === 'paper' && computerSelection === 'rock') {
-        userScore + 1;
-        return("You win! Paper beats Rock.")
-    } else if(playerSelection === 'scissor' && computerSelection === 'paper') {
-        userScore + 1;
-        return("You win! Scissors beats Paper.")
-    } else if(playerSelection === computerSelection) {
-        return("It's a tie!")
-    } else {
-        return("Wrong input")
-    }
-    
+function getPlayerChoice() {
+    //user input choice
+    let input = prompt("Choose 'rock', 'paper', or 'scissor'");
+    return input;
 }
-
-function game(userScore, computerScore) {
-    playRound();
-}
-
-
-
-
-console.log(playRound(playerSelection, computerSelection));
-
