f83378e4fd75af1ffa7cc5b9e1899a069ff4d867
Fatesaid <fatesaid56@gmail.com>
2023-08-24 21:21:45 +0200

    added getComputerChoice and getPlayerChoice
    included () in Math.random to fix NaN output

--- rock.js
+++ rock.js
@@ -1,60 +1,23 @@
-const choices = ["rock", "paper", "scissor"];
-
-function game(){
-    //5 rounds of rock/paper/scissor; calculate scoreboard
-    
-        for(let i = 1; i <= 5; i++) {
-            playRound(i);
-        }
-        
-        
-}
-
-    
-
-
-
-function playRound(round) {
-    //code for round results
-    const playerSelection = getPlayerChoice();
-    const computerSelection = getComputerChoice();
-    const winner = checkWinner(playerSelection, computerSelection)
-
-    
-    
-    
-}
-
-function checkWinner(choiceP, choiceC) {
-    if(choiceP === choiceC) {
-        return 'Tie';
-    } else if(
-        choiceP === 'rock' && choiceC === 'scissor' || 
-        choiceP === 'paper' && choiceC === 'rock' ||
-        choiceP === 'scissor' && choiceC === 'paper'
-        ) {
-            return 'Player';
-        } else {
-            return 'Computer';
-    }
-
-}
-
-
-
-function getComputerChoice() {
-    return choices[Math.floor(Math.random() * choices.length)];
-}
+const choice = ['rock', 'paper', 'scissor'];
 
 function getPlayerChoice() {
-    let input = prompt("Choose 'Rock', 'Paper', or 'Scissor'");
-    if(input === 'rock' || input === 'paper' || input === 'scissor') {
-        return input;
+    let playerSelection;
+    playerSelection = prompt("Choose between 'Rock, 'Paper', or 'Scissor'.").toLowerCase();
+    if(playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissor') {
+        return playerSelection;
     } else {
-        console.log('Invalid');
+        console.log("Invalid")
     }
+    
 }
 
-console.log(playRound(choiceP, choiceC));
-console.log(game(playerScore, computerScore));
+function getComputerChoice() {
 
+
+    let computerSelection;
+    computerSelection = Math.floor(Math.random() * 3) + 1;
+    return computerSelection;
+    
+}
+console.log(getPlayerChoice());
+console.log(getComputerChoice())
